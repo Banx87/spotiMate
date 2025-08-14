1d72d54f98628f51d6c40bdcf5fdd1b110022d74
Banx87 <joni.lindholm@gmail.com>
2025-08-14 16:34:33 +0300

    [ADD] Prepare route protection with clerk middleware

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import { clerkMiddleware } from "@clerk/express";
 import { connectDB } from "./lib/db.js";
 
 import userRoutes from "./routes/user.route.js";
@@ -15,6 +16,8 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
+
+app.use(clerkMiddleware()); // Add auth to req.object
 
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
