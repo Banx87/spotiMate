3ddc10159d395499447da80cfe6ec6db312f1a09
Banx87 <joni.lindholm@gmail.com>
2025-08-18 14:10:37 +0300

    feat: integrate react-router for navigation and add AuthProvider for token management
    - Added axios for API requests and created axios instance with base URL.
    - Refactored App component to use react-router for routing to HomePage and AuthCallbackPage.
    - Created AuthProvider to manage authentication state and update API token.
    - Updated main.tsx to wrap App with BrowserRouter and AuthProvider.
    - Configured Vite server to run on port 3000.

---
+++ frontend/src/providers/AuthProvider.tsx
@@ -0,0 +1,44 @@
+import { useAuth } from "@clerk/clerk-react";
+import { useEffect, useState } from "react";
+import { axiosInstance } from "../lib/axios";
+import { Loader2 } from "lucide-react";
+
+const updateApiToken = (token: string | null) => {
+	if (token)
+		axiosInstance.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+	else delete axiosInstance.defaults.headers.common["Authorization"];
+};
+
+const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+	const { getToken, userId } = useAuth();
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		const initAuth = async () => {
+			try {
+				const token = await getToken();
+				updateApiToken(token);
+
+				//TODO: if(token)
+			} catch (error: any) {
+				updateApiToken(null);
+				console.log("ERROR IN AUTHPROVIDER", error);
+			} finally {
+				setLoading(false);
+			}
+		};
+
+		initAuth();
+	}, [getToken]);
+
+	if (loading)
+		return (
+			<div className="h-screen w-full flex items-center justify-center">
+				<Loader2 className="size-8 text-emerald-500 animate-spin" />
+			</div>
+		);
+
+	return <>{children}</>;
+};
+
+export default AuthProvider;
