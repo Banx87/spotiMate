0005ca0a4d736e99ee983bc262f4c41fed81e167
Banx87 <joni.lindholm@gmail.com>
2025-08-20 14:43:20 +0300

    feat: update Topbar component with logo and admin link; refactor authentication handling

--- frontend/src/components/Topbar.tsx
+++ frontend/src/components/Topbar.tsx
@@ -1,30 +1,38 @@
-import { SignedIn, SignedOut, SignOutButton } from "@clerk/clerk-react";
+import { SignedOut, UserButton } from "@clerk/clerk-react";
 import { LayoutDashboardIcon } from "lucide-react";
 import { Link } from "react-router-dom";
 import SignInOAuthButtons from "./SignInOAuthButtons";
+import { useAuthStore } from "../stores/useAuthStore";
+import { buttonVariants } from "./ui/button";
+import { cn } from "../lib/utils";
 
 const Topbar = () => {
-	const isAdmin = false;
+	const isAdmin = useAuthStore();
 	return (
-		<div className="flex items-center justify-between p-4 sticky top-0 bg-zinc-900/75 backdrop-blur-md z-10">
+		<div className="flex items-center justify-between p-4 pl-0 sticky top-0 max-h-[88px] bg-lime-300/85  backdrop-blur-md z-10">
 			<div className="flex gap-2 items-center">
-				<span className="text-white">SpotiMate</span>
+				<img src="/SpotiMate_logo.png" className=" h-[125px]" />
 			</div>
-			<div className="flex items-center gap-4">
+			<div className="flex items-center gap-4 mr-2 ">
 				{isAdmin && (
-					<Link to="/admin" className="text-white">
-						<LayoutDashboardIcon className="size-4 mr-2" />
+					<Link
+						to="/admin"
+						className={cn(
+							buttonVariants({
+								variant: "black",
+							})
+						)}
+					>
+						<LayoutDashboardIcon className=" size-4 mr-2" />
 						Admin Dashboard
 					</Link>
 				)}
 
-				<SignedIn>
-					<SignOutButton />
-				</SignedIn>
-
 				<SignedOut>
 					<SignInOAuthButtons />
 				</SignedOut>
+
+				<UserButton />
 			</div>
 		</div>
 	);
